fd890f9c49cd5ef9bb9bd460ef471e3f6b30f90a
ramakrishnaprasadS <121850384+ramakrishnaprasadS@users.noreply.github.com>
2023-02-17 08:58:59 +0530

    ddl updated

--- projectHealthCare/DDL.sql
+++ projectHealthCare/DDL.sql
@@ -1,26 +1,5 @@
 -- Active: 1671786369069@@127.0.0.1@3308@healthcare
 
-
-/*
- Tables_in_healthcare |
-+----------------------+
-| address              |
-| claim                |
-| contain              |
-| disease              |
-| insurancecompany     |
-| insuranceplan        |
-| keep                 |
-| keep_log             |
-| medicine             |
-| new_places           |
-| patient              |
-| person               |
-| pharmacy             |
-| prescription         |
-| treatment            |
-
-*/
 use HealthCare;
 
 
